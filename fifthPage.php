1f64fa07c93f6a8cf942e28ee3f004e7fc6194cc
kevinli8831 <kevinli8831@Gmail.com>
2020-11-28 01:46:38 +0800

    [fifthPage] working on fifthPage List

--- fifthPage.php
+++ fifthPage.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,15 +11,43 @@ session_start();
 
 
 <body>
-fifthPage !!!
+<div class="container">
+    <h1>Shopping Cart <?php echo sizeof($_SESSION['selectedProduct']) ?> items</h1>
 
-<?php
-// echo "selected=". $_SESSION['selectedFood'];
-?>
+    <?php
+    foreach($_SESSION['selectedProduct'] as $key=>$value)
+    {
+        if ($_SESSION['globalProduct'][$key][2]==$value)
+        {
+            echo '<div class="row">';
+         echo '<span style="display: inline-block;min-width: 320px">'.$_SESSION['globalProduct'][$key][0].'</span>';
+          echo "<span style='color: #E5483F'>".'$'.$_SESSION['globalProduct'][$key][1].'</span><br>';
+        echo '<hr></div>';
+        }
+    }
+    ?>
+    <div>
+        <span style="display: inline-block;min-width: 316px">Total Money:</span>
+        <?php
+        $temp=0;
+        foreach ($_SESSION['selectedProduct'] as $key=>$value)
+        {
+            if ($_SESSION['globalProduct'][$key][2]==$value)
+            {
+                $temp=$temp+$_SESSION['globalProduct'][$key][1];
+            }
+        }
+        echo '<span style="color: #E5483F">'.'$'.$temp.'</span>';
+        ?>
+    </div>
+</div>
+
 
 </body>
 
 <style>
-
+.container{
+    margin: 0px 25%;
+}
 </style>
 </html>
