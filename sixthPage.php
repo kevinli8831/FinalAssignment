b7cda438e21701126597ec5d4ae382904ce44878
kevinli8831 <kevinli8831@Gmail.com>
2020-12-01 18:36:27 +0800

    revert sixthPage PHP code

--- sixthPage.php
+++ sixthPage.php
@@ -1,154 +1,187 @@
-<html><head><style data-styles="">ion-icon{visibility:hidden}.hydrated{visibility:inherit}</style>
-<title>sixthPage</title>
-<script type="module" src="https://unpkg.com/ionicons@5.2.3/dist/ionicons/ionicons.esm.js" data-stencil-namespace="ionicons"></script><script nomodule="" src="https://unpkg.com/ionicons@5.2.3/dist/ionicons/ionicons.js" data-stencil-namespace="ionicons"></script></head><body>
+<?php
+session_set_cookie_params(0);
+session_start();
 
-<div class="top-bar">
-    <div class="logo">
-        <a href="firstPage.php">
-            <img src="logo2.png">
-        </a>
+$servername = "localhost";
+$username = "u1";
+$password = "a1";
+$dbname = "jp";
+$name="";
+$price=0;
+$id=0;
 
-
-    </div>
-    <div class="menu">
-        <a href="firstPage.php" style="color: #ffc111;font-size: 25px;text-decoration: none">Home</a>
-        <a href="fifthPage.php" class="cart"><ion-icon name="cart-outline" style="font-size: 25px;align-self: end;" role="img" class="md hydrated" aria-label="cart outline"></ion-icon>
-            &nbsp;<span id="cartItems"> Cart 0</span></a>
-    </div>
+$connect = new mysqli($servername, $username, $password);
 
 
-</div>
+$sql = "CREATE DATABASE jp";
+// 本身佢無DB 就會CREATE
+if ($connect->query($sql) === TRUE) {
+    echo "Created DATABASE jp";
+    $sql = "CREATE TABLE product (
+            name VARCHAR(30),
+            price INT(30),
+            id INT(30)
+            )";
+    $connectDB = new mysqli($servername, $username, $password,$dbname);
+
+    if ($connectDB->query($sql) === TRUE) {
+        echo "Table product created successfully";
+
+        if(isset($_POST['pay']))
+            $pay=$_POST['pay'];
+
+        if(isset($pay))
+        {
+            if(isset($_SESSION['globalProduct'])&&$_SESSION['selectedProduct'])
+                for($i=0;$i<sizeof($_SESSION['globalProduct']);$i++)
+                {
+                    for($a=0;$a<sizeof($_SESSION['selectedProduct']);$a++)
+                    {
+
+                        if ($_SESSION['selectedProduct'][$a]==$_SESSION['globalProduct'][$i][2])
+                        {
+                            $name=$_SESSION['globalProduct'][$i][0];
+                            $price=$_SESSION['globalProduct'][$i][1];
+                            $id=$_SESSION['globalProduct'][$i][2];
+                            $sql = "INSERT INTO product (name, price, id) VALUES ('$name', '$price', '$id')";
+
+                            if ($connectDB->query($sql) === TRUE) {
+                                echo "新记录插入成功";
+                            } else {
+                                echo "Error: " . $sql . "<br>" . $connectDB->error;
+                            }
+                        }
+                    }
+                }
+            else
+                echo "globalProduct and selectedProduct not exists.";
+
+        }
 
 
-<br>
-<b>Warning</b>:  mysqli::__construct(): (HY000/2002): 無法連線，因為目標電腦拒絕連線。
- in <b>C:\xampp\htdocs\WebFinalAssignment\sixthPage.php</b> on line <b>18</b><br>
-<br>
-<b>Warning</b>:  mysqli::query(): Couldn't fetch mysqli in <b>C:\xampp\htdocs\WebFinalAssignment\sixthPage.php</b> on line <b>23</b><br>
-<br>
-<b>Warning</b>:  mysqli::query(): Couldn't fetch mysqli in <b>C:\xampp\htdocs\WebFinalAssignment\sixthPage.php</b> on line <b>81</b><br>
-<br>
-<b>Warning</b>:  main(): Couldn't fetch mysqli in <b>C:\xampp\htdocs\WebFinalAssignment\sixthPage.php</b> on line <b>134</b><br>
-Error<br>
-<b>Warning</b>:  mysqli::close(): Couldn't fetch mysqli in <b>C:\xampp\htdocs\WebFinalAssignment\sixthPage.php</b> on line <b>169</b><br>
 
 
 
-    
 
-<div style="text-align:center; line-height:600px">
-<b><font size="7" ;="" color="#0000FF">
-Thank You</font></b></div>
+
+
+
+    } else {
+        echo "Table product created failed: " . $connect->error;
+    }
+}
+// 如果佢有DB 就DELETED 再CREATED
+else {
+    $sql = 'DROP DATABASE jp';
+    if ($connect->query($sql) === TRUE) {
+        echo "deleted DATABASE jp"."<br>";
+        $sql = "CREATE DATABASE jp";
+
+
+        if ($connect->query($sql) === TRUE){
+            echo "Created DATABASE jp";
+            $sql = "CREATE TABLE product (
+                    name VARCHAR(30),
+                    price INT(30),
+                    id INT(30)
+                    )";
+            $connectDB = new mysqli($servername, $username, $password,$dbname);
+
+            if ($connectDB->query($sql) === TRUE) {
+                echo "Table product created successfully";
+                if(isset($_POST['pay']))
+                    $pay=$_POST['pay'];
+
+                if(isset($pay))
+                {
+                    if(isset($_SESSION['globalProduct'])&&$_SESSION['selectedProduct'])
+                        for($i=0;$i<sizeof($_SESSION['globalProduct']);$i++)
+                        {
+                            for($a=0;$a<sizeof($_SESSION['selectedProduct']);$a++)
+                            {
+
+                                if ($_SESSION['selectedProduct'][$a]==$_SESSION['globalProduct'][$i][2])
+                                {
+                                    $name=$_SESSION['globalProduct'][$i][0];
+                                    $price=$_SESSION['globalProduct'][$i][1];
+                                    $id=$_SESSION['globalProduct'][$i][2];
+                                    $sql = "INSERT INTO product (name, price, id) VALUES ('$name', '$price', '$id')";
+
+                                    if ($connectDB->query($sql) === TRUE) {
+                                        echo "新记录插入成功";
+                                    } else {
+                                        echo "Error: " . $sql . "<br>" . $connectDB->error;
+                                    }
+                                }
+                            }
+                        }
+                    else
+                        echo "globalProduct and selectedProduct not exists.";
+
+                }
+
+            } else {
+                echo "Table product created failed: " . $connect->error;
+            }
+        }
+
+    } else {
+        echo "Error" . $connect->error;
+    }
+}
+
+//    if(isset($_POST['pay']))
+//        $pay=$_POST['pay'];
+//
+//    if(isset($pay))
+//    {
+//        if(isset($_SESSION['globalProduct'])&&$_SESSION['selectedProduct'])
+//            for($i=0;$i<sizeof($_SESSION['globalProduct']);$i++)
+//            {
+//                for($a=0;$a<sizeof($_SESSION['selectedProduct']);$a++)
+//                {
+//
+//                    if ($_SESSION['selectedProduct'][$a]==$_SESSION['globalProduct'][$i][2])
+//                    {
+//                        $name=$_SESSION['globalProduct'][$i][0];
+//                        $price=$_SESSION['globalProduct'][$i][1];
+//                        $id=$_SESSION['globalProduct'][$i][2];
+//                        $sql = "INSERT INTO product (name, price, id) VALUES ('$name', '$price', '$id')";
+//
+//                        if ($connect->query($sql) === TRUE) {
+//                            echo "新记录插入成功";
+//                        } else {
+//                            echo "Error: " . $sql . "<br>" . $connect->error;
+//                        }
+//                    }
+//                }
+//            }
+//        else
+//            echo "globalProduct and selectedProduct not exists.";
+//
+//}
+
+$connect->close();
+
+?>
+
+<html>
+<head>
+    <title>sixthPage</title>
+</head>
+<body>
+
+
+
+</body>
+
+
+<script>
+
+</script>
+
 
 <style>
-    body{
-        background-image: url("background.jpg");
-        background-size: auto;
-        margin: 0px;
-    }
-    .display-inlineBlock{
-        display: inline-block;
-        background-color: #fff;
-    }
-    .menu{
-        align-self: center;
-        margin-right: 30px;
 
-    }
-    .top-bar{
-        background-color: #111111;
-        height: 70px;
-        border-bottom: 3px solid #ffc111;
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .logo img{
-        width: 160px;
-        height: 70px;
-        float: left;
-    }
-    .cart{
-        color: #ffc111;
-        display: inline-flex;
-        align-items: baseline;
-        font-size: 25px;
-        text-decoration: none;
-    }
-    .menu-bar {
-        float: right;
-    }
-    .menu-bar ico-icon{
-        vertical-align: bottom;
-        font-size: 20px;
-        float: right;
-    }
-    .header{
-        text-align: center;
-        margin: 5px 0px;
-    }
-    .button{
-        width: 105px;
-        height: 35px;
-        border-radius: 10px;
-        border-color: #AF7255;
-    }
-    .title img{
-		width: 200px;
-        height: 100px;
-	}
-
-    .previous{
-        float: left;
-        margin-left: 20px;
-
-    }
-
-    .next{
-        float: right;
-        margin-right: 20px;
-
-    }
-
-    .menu-bar li{
-        list-style: none;
-        display: inline-block;
-        padding-right: 10px;
-        padding-left: 10px;
-    }
-    .myMOUSE {
-        cursor: pointer;
-    }
-    .myMOUSE:hover {
-       background-color: coral;
-    }
-    .menu-bar spna{
-        padding-left: 5px;
-    }
-
-    .menu-bar a{
-        padding: 5px;
-        color: #ffc111;
-        text-decoration: none;
-    }
-
-    img {
-        width: 300px;
-        height: 300px;
-    }
-
-    .products{
-
-        padding: 10px;
-        background-color: cornsilk;
-        border-radius: 15px;
-        border: 5px solid #AF7255;
-    }
-    .row {
-        display: flex;
-        justify-content: space-evenly;
-        margin-bottom: 30px;
-    }
 </style>
-
-</body></html>
+</html>
