ff2091994ced238dea9faa1ec49f0b9aeb0adcdc
LAPTOP-RIOB1PI1\H.Ling <joannchl2000@yahoo.com.hk>
2020-12-01 13:49:27 +0800

    no message

---
+++ sixthPage.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>sixthPage</title>
+</head>
+<body>
+
+
+<div style='text-align:center; line-height:600px'>
+<b><font size="7"; color="#0000FF">
+<?php echo "Thank You";?>
+</font></b></div>
+
+<style>
+	.container{
+    margin: 0px 25%;
+	padding: 10px;
+        background-color: cornsilk;
+        border-radius: 15px;
+        border: 5px solid #AF7255;
+	}
+	
+	body{
+		background-image: url("background.jpg");
+		background-size: auto;
+		}
+		
+	
+	
+</style>
+
+</body>
+</html>
